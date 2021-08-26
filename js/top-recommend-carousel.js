66372cd2d78a12f06cd05d2d667cb207b80019d1
Den Bondarenko <denbond24@gmail.com>
2021-08-26 19:38:42 +0000

    top-recommend-carousel

--- js/top-recommend-carousel.js
+++ js/top-recommend-carousel.js
@@ -1,24 +1,80 @@
+// (function () {
+//     const slides = [
+//         `<img src="img/dog-fun-set.png" alt="doggy fun set">`,
+//         `<img src="img/pillow-bed.png" alt="duna pillow bed">`,
+//         `<img src="img/retractable-leach.png" alt="aga retractable leach">`,
+//         `<img src="img/doggy-2-bunch-toy.png" alt="doggy 2 bunch toy">`
+//     ];
+
+//     let currentSlideIdx = 0;
+
+//     function showCurrentSlide () {
+//         const slideContainer = document.querySelector('.carousel-prod .top-slide');
+//         slideContainer.innerHTML = slides[currentSlideIdx];
+//     }
+
+//     function nextSlide () {
+//         currentSlideIdx++;
+//         if (currentSlideIdx > slides.length) currentSlideIdx = 0;
+//         showCurrentSlide();
+//     }
+
+//     setInterval(nextSlide, 4000);
+//     showCurrentSlide();
+// });
+
+// const navBar = document.querySelector('.top-recommend-text');
+// navBar.innerText += '!!!!!!@@@@'; 
+// navBar.style.color = 'red';
+setInterval(updateTime, 1000);
+
+function updateTime() {
+    const clockContainer = document.querySelector('.top-clock');
+    clockContainer.innerText = new Date().toLocaleTimeString();
+}
+
 (function () {
-    const slides = [
-        `<img src="img/dog-fun-set.png" alt="doggy fun set">`,
-        `<img src="img/pillow-bed.png" alt="duna pillow bed">`,
-        `<img src="img/retractable-leach.png" alt="aga retractable leach">`,
-        `<img src="img/doggy-2-bunch-toy.png" alt="doggy 2 bunch toy">`
-    ];
+    const productJson =`[
+        {
+            "id": "1",
+            "name": "DOGGY Fun Set",
+            "price": "6,00",
+            "imgUrl": "img/dog-fun-set.png"
+        },
+        {
+            "id": "2",
+            "name": "DUNA Pillow Bed",
+            "price": "10,00",
+            "imgUrl": "img/pillow-bed.png"
+        },
+        {
+            "id": "3",
+            "name": "AGA Retractable Leash",
+            "price": "33,00",
+            "imgUrl": "img/retractable-leach.png"
+        },
+        {
+            "id": "4",
+            "name": "DOGGY 2-Bunch Toy",
+            "price": "5,00",
+            "imgUrl": "img/doggy-2-bunch-toy.png"
+        }
+        
+    ]`;
 
-    let currentSlideIdx = 0;
+    const products = JSON.parse(productJson);
 
-    function showCurrentSlide () {
-        const slideContainer = document.querySelector('.carousel-prod .top-slide');
-        slideContainer.innerHTML = slides[currentSlideIdx];
+    let productsHtml = '';     
+    for (const product of products) {
+        productsHtml += `
+            <div class="top-goods">
+                <a href="doggy-fun-set"><img src="${product.imgUrl}" alt="${product.name}"></a>
+                <a class="top-good-name" href="doggy-fun-set">${product.name}</a>
+                <p>$${product.price}</p>
+                <a class="button" href="add-to-cart">Add to Card</a>
+            </div>`;
     }
 
-    function nextSlide () {
-        currentSlideIdx++;
-        if (currentSlideIdx > slides.length) currentSlideIdx = 0;
-        showCurrentSlide();
-    }
+    document.querySelector('.carousel-prod').innerHTML = productsHtml;
 
-    setInterval(nextSlide, 4000);
-    showCurrentSlide();
-});
+})();
