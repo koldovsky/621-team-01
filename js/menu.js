398694af1a4b2aa86c421b2cce9179d5cd96b51c
Koritska Aklona <88449466+Koritska7@users.noreply.github.com>
2021-08-31 20:29:13 +0300

    Mobile Version JS - Burger Menu

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,35 @@
-const openMobileMenuButton = document.querySelector('.open-mobile-menu-js');
-const closeMobileMenuButton = document.querySelector('.close-mobile-menu-js');
-const mobileMenu = document.querySelector('.title-page');
+(function() {
 
-openMobileMenuButton.addEventListener('click', showMobileMenu);
-closeMobileMenuButton.addEventListener('click', hideMobileMenu);
+    const openMobileMenuButton = document.querySelector('.open-mobile-menu-js');
+    const closeMobileMenuButton = document.querySelector('.close-mobile-menu-js');
+    const mobileMenu = document.querySelector('.title-page');
 
-function showMobileMenu(){
-    openMobileMenuButton.style.display = 'none';
-    closeMobileMenuButton.style.display = 'block';
-    mobileMenu.style.display = 'block';
-}
+    openMobileMenuButton.addEventListener('click', showMobileMenu);
+    closeMobileMenuButton.addEventListener('click', hideMobileMenu);
 
-function hideMobileMenu(){
-    openMobileMenuButton.style.display = 'block';
-    closeMobileMenuButton.style.display = 'none';
-    mobileMenu.style.display = 'none';
-}
+    function showMobileMenu(){
+        openMobileMenuButton.style.display = 'none';
+        closeMobileMenuButton.style.display = 'block';
+        mobileMenu.style.display = 'block';
+    }
+
+    function hideMobileMenu(){
+        openMobileMenuButton.style.display = 'block';
+        closeMobileMenuButton.style.display = 'none';
+        mobileMenu.style.display = 'none';
+    }
+
+    function showBurgerControls() {
+        if (window.innerWidth < 990) {
+            openMobileMenuButton.style.display = 'block';
+            mobileMenu.style.display = 'none';
+        } else {
+            openMobileMenuButton.style.display = 'none';
+            mobileMenu.style.display = 'flex';
+        }
+    }
+
+    showBurgerControls();
+    window.addEventListener('resize', showBurgerControls);
+    
+})();
